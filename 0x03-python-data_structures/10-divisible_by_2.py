95676bac34e12edb5e7dff538a483cfb48dc9f80
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-10-10 19:18:11 -0700

    task 10 fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
     result = []
-    if len(my_list) < 1:
-        return none
     for i in my_list:
         if i % 2 == 0:
             result.append(True)
