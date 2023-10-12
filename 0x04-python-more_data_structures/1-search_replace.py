31c7544f1da93ec0fce896597dd859e7b7f096c3
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-10-11 17:35:25 -0700

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if not my_list:
+        return None
+    list_copy = my_list.copy()
+    for i in range(len(list_copy)):
+        if list_copy[i] == search:
+            list_copy[i] = replace
+    return list_copy
