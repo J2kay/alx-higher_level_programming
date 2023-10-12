0b3fbc8be4ed0a3e2cdf2f0bd94139e98b54ab98
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-10-11 17:42:07 -0700

    task 1 fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,11 @@ def search_replace(my_list, search, replace):
     if not my_list:
         return None
     list_copy = my_list.copy()
+    check = False
     for i in range(len(list_copy)):
         if list_copy[i] == search:
             list_copy[i] = replace
+            check = True
+    if not check:
+        return None
     return list_copy
