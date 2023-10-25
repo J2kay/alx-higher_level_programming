364c7de44fa026300fc200fb8c43399ab55f4ba9
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-10-24 19:58:14 -0700

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This is a hash bang header"""
+
+
+class Square:
+    """This is a class that defines a square"""
+
+    def __init__(self, size=0):
+        """This method initializes the object with the specified
+            values for parameter1.
+            It sets these values as instance variables for the object"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """This method returns the current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """retrieves the property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter method for the size property"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be an integer")
+        self.__size = value
