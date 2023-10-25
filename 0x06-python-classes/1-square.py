c3abe2e817194555301431714eaad963a379763f
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-10-24 18:58:02 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is a hash bang header"""
+
+
+class Square:
+    """This is a class that defines a square"""
+
+    def __init__(self, size=None):
+        """This method initializes the object with the specified
+            values for parameter1.
+            It sets these values as instance variables for the object"""
+        self.__size = size
