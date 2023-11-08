c904ecc9fcf852709b783c69079ade5172391296
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-11-07 17:54:24 -0800

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a function that appends a string at the end of a text file"""
+
+
+def append_write(filename="", text=""):
+    """a function that appends a string at the end of a text file"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        c_add = f.write(text)
+    return c_add
