f9d1ec3bd82feae797e23d631e82940a5df46b1f
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-11-07 19:46:37 -0800

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a script that adds all arguments to a Python list,
+    and then save them to a file"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+my_args = list(sys.argv[1:])
+
+try:
+    data1 = load_from_json_file('add_item.json')
+except Exception:
+    data1 = []
+
+data1.extend(my_args)
+save_to_json_file(data1, 'add_item.json')
