c57ea8d1a39954b9bb760f0c5534a30e6b26b0e0
Jeffrey Karikari <jeffreyk31k@gmail.com>
2023-11-13 18:29:46 -0800

    # square

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,8 @@ class Rectangle(Base):
     def area(self):
         """Return the area of the Rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """Print the Rectangle using '#' characters"""
+        for i in range(self.__height):
+            print("#" * self.__width)
